14291fe26cd3652789e2a24976513bd8b018c21e
YOUR NAME <Maria>
2014-10-30 12:31:34 +0100

    Initial Commit

---
+++ SmartGridApp/SmartGridApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartGridApp
+//
+//  Created by Maria on 30/10/14.
+//  Copyright (c) 2014 Maria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
